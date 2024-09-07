f32af0592e24e95d6d72129a0bda3116fa95e69f
demarchek <demarchek11@mail.ru>
2024-09-07 10:28:31 +0300

    Task170341. Добавил компонент в визуальный редактор, а также фразы из шаблона в языковой файл

--- local/components/mcart/agents.list/.description.php
+++ local/components/mcart/agents.list/.description.php
@@ -8,22 +8,12 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 
 $arComponentDescription = array(
-	"NAME" => "mcart:agents.list",
-	"DESCRIPTION" => "компонент от mcart",
-//	"SCREENSHOT" => array(
-//		"/images/post-77-1108567822.jpg",
-//		"/images/post-1169930140.jpg",
-//	),
-	// "CACHE_PATH" => "Y",
-	// "PATH" => array(
-	// 	"ID" => "content",
-	// 	"CHILD" => array(
-	// 		"ID" => "news",
-	// 		"NAME" => GetMessage("T_IBLOCK_DESC_NEWS"),
-	// 		"SORT" => 10,
-	// 		"CHILD" => array(
-	// 			"ID" => "news_cmpx",
-	// 		),
-	// 	),
-	// ),
+	"NAME" => GetMessage("AGENTS_LIST_COMPONENT"),
+	"DESCRIPTION" => GetMessage("COMPONENT_DESC"),
+	"PATH" => array(
+		"ID" => "MCART_COMPONENTS",
+		"NAME" => GetMessage("MCART_COMPONENTS"),
+		"SORT" => 1	
+	),
+
 );
